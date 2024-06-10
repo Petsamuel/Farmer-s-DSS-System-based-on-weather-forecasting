b6306d851f7f39ef014146b299fca07ff739227f
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-10 21:54:25 +0100

    implemented chart

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,37 @@ const dummyWeatherData = {
     temp: 22,
     humidity: 45,
     wind_speed: 19.2,
-    dt: 1606137900,
+    dt: 1718121600, // UNIX timestamp for June 10, 2024
   },
   daily: [
     {
-      dt: 1606137900,
+      dt: 1718208000, // June 11, 2024
       temp: { day: 22 },
       humidity: 30,
       weather: [{ description: "Cloudy" }],
     },
     {
-      dt: 1606224300,
+      dt: 1718294400, // June 12, 2024
       temp: { day: 24 },
       humidity: 36,
       weather: [{ description: "Partly Cloudy" }],
     },
     {
-      dt: 1606310700,
+      dt: 1718380800, // June 13, 2024
       temp: { day: 26 },
       humidity: 20,
       weather: [{ description: "Sunny" }],
     },
     {
-      dt: 1606397100,
+      dt: 1718467200, // June 14, 2024
       temp: { day: 28 },
       humidity: 15,
       weather: [{ description: "Sunny" }],
     },
   ],
 };
+
+console.log(dummyWeatherData);
 
 
 function App() {
