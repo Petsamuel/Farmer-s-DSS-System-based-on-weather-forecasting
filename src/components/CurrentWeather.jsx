e22f4940ecadb7274eeeb73a2b0e753cd1dcb6d3
Dhavisco <olaniyidavid2002@gmail.com>
2024-06-14 16:01:51 +0100

    added svg icons

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,10 +1,34 @@
 import PropTypes from "prop-types";
+import weatherIcons from "../store/data.json"
+// import cloudyIcon from "../assets/icons/cloudy.svg"; 
+// import rainIcon from "../assets/icons/rain.svg"
+// import clearIcon from "../assets/icons/clear-day.svg"
+
 
 const CurrentWeather = (props) => {
- const { weather, main, wind, dt } = props.weatherData;
+  const { weather, main, wind, dt } = props.weatherData;
+  // const weatherIconMap = {
+  //   Clouds: cloudyIcon,
+  //   Rain: rainIcon,
+  //   Clear: clearIcon,
+    
+  //  
+  // };
+
+  const weatherDescription = weather[0].description;
+  const weatherMain = weather[0].main;
+  const weatherIcon = weatherIcons[weatherMain] || weatherIcons['Clear']; // Fallback to a default icon if not mapped
+
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">{weather[0].description}</h2>
+      <h2 className="text-2xl font-bold mb-4 flex items-center">
+        {weatherDescription}
+        <img
+          src={weatherIcon}
+          alt={weatherDescription}
+          className="w-8 h-8 ml-2"
+        />
+      </h2>
       <div className="flex justify-between items-center">
         <div>
           <div className="text-6xl">{Math.round(main.temp)}°C</div>
@@ -24,6 +48,7 @@ CurrentWeather.propTypes = {
     weather: PropTypes.arrayOf(
       PropTypes.shape({
         description: PropTypes.string.isRequired,
+        main: PropTypes.string.isRequired,
       })
     ).isRequired,
     main: PropTypes.shape({
@@ -36,6 +61,5 @@ CurrentWeather.propTypes = {
     dt: PropTypes.number.isRequired,
   }).isRequired,
 };
-
 
 export default CurrentWeather;
